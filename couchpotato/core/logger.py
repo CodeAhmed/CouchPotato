fd62c96da77d8170f7a6af3a7d7424074f6a9d0c
Mansoor Ahmed <mansooronly4u@gmail.com>
2024-09-29 22:49:16 -0400

    Tracebacks to file

--- couchpotato/core/logger.py
+++ couchpotato/core/logger.py
@@ -1,14 +1,21 @@
-from couchpotato import app
+import logging
 class CPLog():
     context = ''
     def __init__(self, context = ''):
         self.context = context
-        self.logger = app.logger
+        self.logger = logging.getLogger()
+
     def info(self, msg):
         self.logger.info(self.addContext(msg))
+
     def debug(self, msg):
         self.logger.debug(self.addContext(msg))
+
     def error(self, msg):
         self.logger.error(self.addContext(msg))
+
+    def critical(self, msg):
+        self.logger.critical(self.addContext(msg), exc_info = 1)
+
     def addContext(self, msg):
         return '[%+25.25s] %s' % (self.context[-25:], msg)
--- couchpotato/settings/loader.py
+++ couchpotato/settings/loader.py
@@ -2,31 +2,44 @@ from blinker import signal
 from couchpotato.core.logger import CPLog
 import glob
 import os
-import sys
+
 log = CPLog(__name__)
 class SettingsLoader:
+
     def __init__(self, root = ''):
-        self.register = signal('settings_register')
+
+        self.settings_register = signal('settings.register')
+        self.settings_save = signal('settings.save')
+
         self.paths = {
             'plugins' : ('couchpotato.core.plugins', os.path.join(root, 'couchpotato', 'core', 'plugins')),
             'providers' : ('couchpotato.core.providers', os.path.join(root, 'couchpotato', 'core', 'providers')),
         }
         for type, tuple in self.paths.items():
             self.loadFromDir(tuple[0], tuple[1])
+
     def loadFromDir(self, module, dir):
+        did_save = 0
         for file in glob.glob(os.path.join(dir, '*')):
             plugin_name = os.path.basename(file)
             plugin_dir = os.path.join(dir, plugin_name)
             if os.path.isdir(plugin_dir):
-                self.loadConfig(module, plugin_name)
-    def loadConfig(self, module, name):
+                did_save += self.loadConfig(module, plugin_name, save = False)
+            
+            if did_save:
+                self.settings_save.send()
+
+    def loadConfig(self, module, name, save = True):
         module_name = '%s.%s' % (module, name)
+        print (module_name)
         try:
             m = getattr(self.loadModule(module_name), name)
             (section, options) = m.config
-            self.register.send(section, options = options)
-        except:
-            log.error("Failed loading config for %s" % name)
+            self.settings_register.send(section, options = options, save = save)
+            return True
+        except (Exception, e):
+            log.error("Failed loading config for %s: %s" % (name, e))
+            
     def loadModule(self, name):
         try:
             m = __import__(name)
