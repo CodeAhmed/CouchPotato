d90b8cbd76d4cf751dd347df5642cb1f6c7dc344
Mansoor Ahmed <mansooronly4u@gmail.com>
2024-09-29 23:07:31 -0400

    Save all settings at once. Removed Log Directory module

--- couchpotato/settings/loader.py
+++ couchpotato/settings/loader.py
@@ -6,6 +6,8 @@ import os
 log = CPLog(__name__)
 class SettingsLoader:
 
+    configs = {}
+
     def __init__(self, root = ''):
 
         self.settings_register = signal('settings.register')
@@ -16,18 +18,22 @@ class SettingsLoader:
             'providers' : ('couchpotato.core.providers', os.path.join(root, 'couchpotato', 'core', 'providers')),
         }
         for type, tuple in self.paths.items():
-            self.loadFromDir(tuple[0], tuple[1])
+            self.addFromDir(tuple[0], tuple[1])
 
-    def loadFromDir(self, module, dir):
+    def run(self):
         did_save = 0
-        for file in glob.glob(os.path.join(dir, '*')):
-            plugin_name = os.path.basename(file)
-            plugin_dir = os.path.join(dir, plugin_name)
-            if os.path.isdir(plugin_dir):
-                did_save += self.loadConfig(module, plugin_name, save = False)
+        
+        for module, plugin_name in self.configs.items():
+            did_save += self.loadConfig(module, plugin_name, save = False)
             
             if did_save:
                 self.settings_save.send()
+
+    def addFromDir(self, module, dir):
+        for file in glob.glob(os.path.join(dir, '*')):
+            name = os.path.basename(file)
+            if os.path.isdir(os.path.join(dir, name)):
+                self.addConfig(module, name)
 
     def loadConfig(self, module, name, save = True):
         module_name = '%s.%s' % (module, name)
@@ -39,7 +45,11 @@ class SettingsLoader:
             return True
         except (Exception, e):
             log.error("Failed loading config for %s: %s" % (name, e))
-            
+            return False
+
+    def addConfig(self, module, name):
+        self.configs[module] = name
+
     def loadModule(self, name):
         try:
             m = __import__(name)
